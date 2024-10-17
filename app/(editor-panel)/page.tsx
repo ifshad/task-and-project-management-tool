36d57b819795ba83b70f6f32174acf6da32335a3
ifshad <shoronshark@gmail.com>
2024-10-17 15:00:45 +0600

    logs cleared

--- app/(editor-panel)/page.tsx
+++ app/(editor-panel)/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   const boards: Board[] = await getBoards();
   const tasks: Task[] = await getTasks();
   const users: User[] = await getUsers();
-  console.log(boards, tasks, users);
+  // console.log(boards, tasks, users);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1>Length of boards: {boards.length}</h1>
@@ -18,6 +18,7 @@ export default async function Home() {
         </div>
       ))}
       <h1>Length of tasks: {tasks.length}</h1>
+      <h1>Length of tasks: {users.length}</h1>
       {tasks.map((task) => (
         <div key={task.id}>
           <h1>name: {task.title}</h1>
