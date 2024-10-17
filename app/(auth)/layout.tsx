0f379f7ae235525323c74512aabc2bd4fa6607bd
ifshad <shoronshark@gmail.com>
2024-10-17 13:20:19 +0600

    routes setup updated

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import "../globals.css";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "TOEDO | Login",
+  description: "Enter the management universe!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ app/(editor-panel)/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import "../globals.css";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "TOEDO | Task Manager",
+  description: "Manage Task & Projects easily!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
